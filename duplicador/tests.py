149924ac7f4cb051185c2327c973c98dbe2293fa
ez2torta <ez2torta@gmail.com>
2016-05-06 16:19:05 +0000

    Recurso de Valor número terminado

--- duplicador/tests.py
+++ duplicador/tests.py
@@ -16,4 +16,26 @@ class TestDuplicator(TransactionTestCase):
         self.assertEquals(200, response.status_code)
         self.assertEquals("1",response.content)
         
-    
+    def test_duplicar_valor(self):
+        # Arrange
+        response = self.client.get('/valor')
+        self.assertEquals("1",response.content)
+        
+        # Act
+        response2 = self.client.post('/valor', {'action': 'duplicar'})
+        
+        # Assert
+        self.assertEquals(200, response.status_code)
+        self.assertEquals('2', self.client.get('/valor').content)
+        
+    def test_post_n_duplicate(self):
+        response = self.client.get('/valor')
+        
+        import random
+        times = random.randint(1, 9)
+        for _ in range(times):
+            response = self.client.post('/valor', {'action': 'duplicar'})
+        
+        response2 = self.client.get('/valor')
+        n = int(response2.content)
+        self.assertEquals(2 ** times, n)
